cd16276c0a2104f646fa7a701f464343e290524e
hyassin <haiyassin1701@gmail.com>
2020-07-05 04:25:29 +0200

    Add a some change in Notification Entity

--- src/Entity/Notification.php
+++ src/Entity/Notification.php
@@ -2,9 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\NotificationRepository;
+use App\Model\Content;
+use App\Entity\Repository\NotificationRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity(repositoryClass=NotificationRepository::class)
@@ -21,13 +24,34 @@ class Notification
 
     /**
      * @var string
+     * @Assert\Choice(
+     *     choices={
+     *     "recommendation",
+     *     "newness",
+     *     "shared",
+     *     "information"
+     *     },
+     *     message="Choose a valid type"
+     * )
      *
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=false)
      */
-    private $type;
+    private $contentType;
 
     /**
-     * @var DateTime
+     * @var array
+     *
+     * @ORM\Column(type="array")
+     */
+    private $contentRelated = [];
+
+    /**
+     * @var Content
+     */
+    private $content;
+
+    /**
+     * @var \DateTime
      *
      * @ORM\Column(type="datetime", nullable=true)
      */
@@ -41,17 +65,27 @@ class Notification
     private $description;
 
     /**
-     * @var bool
-     *
-     * @ORM\Column(type="boolean")
+     * @var int
+     * @ORM\Column(name="validity_time")
      */
-    private $view = false;
+    private $validityTime;
 
     /**
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="notifications")
      * @ORM\JoinColumn(nullable=false)
      */
     private $user;
+
+    /**
+     * @ORM\OneToMany(targetEntity="UserNotification", mappedBy="notification")
+     * @ORM\Column(nullable=true)
+     */
+    private $userNotifications;
+
+    public function __construct()
+    {
+        $this->userNotifications = new ArrayCollection();
+    }
 
     /**
      * @return int
@@ -64,37 +98,78 @@ class Notification
     /**
      * @return string
      */
-    public function getType(): string
+    public function getContentType(): string
     {
-        return $this->type;
+        return $this->contentType;
     }
 
     /**
-     * @param string $type
+     * @param string $contentType
      *
      * @return Notification
      */
-    public function setType(string $type): self
+    public function setContentType(string $contentType): self
     {
-        $this->type = $type;
+        $this->contentType = $contentType;
 
         return $this;
     }
 
     /**
-     * @return \DateTimeInterface
+     * @return array
      */
-    public function getCreatedAt(): \DateTimeInterface
+    public function getContentRelated(): array
+    {
+        return $this->contentRelated;
+    }
+
+    /**
+     * @param array $contentRelated
+     *
+     * @return Notification
+     */
+    public function setContentRelated(array $contentRelated): self
+    {
+        $this->contentRelated = $contentRelated;
+
+        return $this;
+    }
+
+    /**
+     * @return Content
+     */
+    public function getContent(): Content
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param Content $content
+     *
+     * @return Notification
+     */
+    public function setContent(Content $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
     {
         return $this->createdAt;
     }
 
     /**
-     * @param \DateTimeInterface $createdAt
+     * @param \DateTime $createdAt
      *
      * @return Notification
      */
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    public function setCreatedAt(\DateTime $createdAt): self
     {
         $this->createdAt = $createdAt;
 
@@ -122,21 +197,21 @@ class Notification
     }
 
     /**
-     * @return bool
+     * @return int
      */
-    public function isView(): bool
+    public function getValidityTime(): int
     {
-        return $this->view;
+        return $this->validityTime;
     }
 
     /**
-     * @param bool $view
+     * @param int $validityTime
      *
      * @return Notification
      */
-    public function setView(bool $view): self
+    public function setValidityTime(int $validityTime): self
     {
-        $this->view = $view;
+        $this->validityTime = $validityTime;
 
         return $this;
     }
@@ -157,6 +232,53 @@ class Notification
     public function setUser(User $user): self
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection|null
+     */
+    public function getUserNotifications()
+    {
+        return $this->userNotifications;
+    }
+
+    /**
+     * @param UserNotification $userNotification
+     *
+     * @return Notification
+     */
+    public function addNUserNotification(UserNotification $userNotification): self
+    {
+        if (!$this->userNotifications->contains($userNotification)) {
+            $this->userNotifications[] = $userNotification;
+            $userNotification->setNotification($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param UserNotification $userNotification
+     *
+     * @return Notification
+     */
+    public function removeUserNotification(UserNotification $userNotification): self
+    {
+        if (!$this->userNotifications->contains($userNotification)) {
+            return $this;
+        }
+
+        $this->userNotifications->removeElement($userNotification);
+        $userNotification->setNotification(null);
+
+        return $this;
+    }
+
+    public function setUserNotifications(?string $userNotifications): self
+    {
+        $this->userNotifications = $userNotifications;
 
         return $this;
     }
