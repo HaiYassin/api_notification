e3cdbda8f029e76cc226e344a0300bfde00c8f47
hyassin <haiyassin1701@gmail.com>
2020-07-05 04:40:10 +0200

    Set up for the next time a helper to calculate the diff between 2 dates

---
+++ src/Helper/DateHelper.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Helper;
+
+/**
+ * Class DateHelper
+ */
+class DateHelper
+{
+    /**
+     * Return a DateTime between Now and another one.
+     *
+     * @param \DateTime $dateTime
+     *
+     * @throws \Exception
+     *
+     * @return \DateTime
+     */
+    public function convertToElapsedTime(\DateTime $dateTime)
+    {
+
+        return new \DateTime();
+    }
+}
