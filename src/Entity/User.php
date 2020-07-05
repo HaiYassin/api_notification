34268c95edb022cf37fb1537755c71891db7dbb6
hyassin <haiyassin1701@gmail.com>
2020-07-05 04:26:01 +0200

    Add some change in User Entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
+use App\Entity\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -28,7 +28,14 @@ class User
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
-    private $profilePictureLink;
+    private $img;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $password;
 
     /**
      * @ORM\Column(type="array")
@@ -37,24 +44,46 @@ class User
 
     /**
      * @ORM\OneToMany(targetEntity=Notification::class, mappedBy="user")
+     * @ORM\Column(nullable=true)
      */
     private $notifications;
 
+    /**
+     * @ORM\OneToMany(targetEntity="UserNotification", mappedBy="notification")
+     * @ORM\Column(nullable=true)
+     */
+    private $userNotifications;
+
+    /**
+     * User constructor.
+     */
     public function __construct()
     {
         $this->notifications = new ArrayCollection();
+        $this->userNotifications = new ArrayCollection();
     }
 
-    public function getId(): ?int
+    /**
+     * @return int
+     */
+    public function getId(): int
     {
         return $this->id;
     }
 
-    public function getName(): ?string
+    /**
+     * @return string
+     */
+    public function getName(): string
     {
         return $this->name;
     }
 
+    /**
+     * @param string $name
+     *
+     * @return User
+     */
     public function setName(string $name): self
     {
         $this->name = $name;
@@ -62,23 +91,59 @@ class User
         return $this;
     }
 
-    public function getProfilePictureLink(): ?string
+    /**
+     * @return string
+     */
+    public function getImg(): string
     {
-        return $this->profilePictureLink;
+        return $this->img;
     }
 
-    public function setProfilePictureLink(?string $profilePictureLink): self
+    /**
+     * @param string $img
+     *
+     * @return User
+     */
+    public function setImg(string $img): self
     {
-        $this->profilePictureLink = $profilePictureLink;
+        $this->img = $img;
 
         return $this;
     }
 
-    public function getRoles(): ?array
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRoles(): array
     {
         return $this->roles;
     }
 
+    /**
+     * @param array $roles
+     *
+     * @return User
+     */
     public function setRoles(array $roles): self
     {
         $this->roles = $roles;
@@ -87,13 +152,18 @@ class User
     }
 
     /**
-     * @return Collection|Notification[]
+     * @return Collection|Notification[]|null
      */
     public function getNotifications(): Collection
     {
         return $this->notifications;
     }
 
+    /**
+     * @param Notification $notification
+     *
+     * @return User
+     */
     public function addNotification(Notification $notification): self
     {
         if (!$this->notifications->contains($notification)) {
@@ -104,15 +174,70 @@ class User
         return $this;
     }
 
+    /**
+     * @param Notification $notification
+     *
+     * @return User
+     */
     public function removeNotification(Notification $notification): self
     {
         if ($this->notifications->contains($notification)) {
             $this->notifications->removeElement($notification);
-
-            if ($notification->getUser() === $this) {
-                $notification->setUser(null);
-            }
         }
+
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection|null
+     */
+    public function getUserNotifications()
+    {
+        return $this->userNotifications;
+    }
+
+    /**
+     * @param UserNotification $userNotification
+     *
+     * @return User
+     */
+    public function addNUserNotification(UserNotification $userNotification): self
+    {
+        if (!$this->userNotifications->contains($userNotification)) {
+            $this->userNotifications[] = $userNotification;
+            $userNotification->setUser($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param UserNotification $userNotification
+     *
+     * @return User
+     */
+    public function removeUserNotification(UserNotification $userNotification): self
+    {
+        if (!$this->userNotifications->contains($userNotification)) {
+            return $this;
+        }
+
+        $this->userNotifications->removeElement($userNotification);
+        $userNotification->setUser(null);
+
+        return $this;
+    }
+
+    public function setNotifications(?string $notifications): self
+    {
+        $this->notifications = $notifications;
+
+        return $this;
+    }
+
+    public function setUserNotifications(?string $userNotifications): self
+    {
+        $this->userNotifications = $userNotifications;
 
         return $this;
     }
